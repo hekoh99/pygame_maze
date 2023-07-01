62728d1ff62b08b46427088679519d08dab84044
haeunko <hekoh99@naver.com>
2023-07-01 19:23:39 +0900

    add mouse event

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+import time
 from pygame.locals import *
 
 ## pygame 기능 사용을 시작하는 명령어 ##
@@ -41,6 +42,12 @@ DOOR_OPENED = pygame.transform.scale(DOOR_OPENED, (60, 60))
 
 ## SOUND
 END_SOUND = pygame.mixer.Sound("birthday_background.mp3")
+
+## 게임 상태 flag
+## 게임 시작 전 : 0
+## 게임 중 : 1
+## 게임 성공 후 : 2
+gameFlag = 0
 
 class Player:
     posX = 0
@@ -102,10 +109,13 @@ class Game:
             self.player.placePlayer()
         else :
             self.gameDisplay.fill(BLACK)
-            GameDisplay.blit(self.player.playerImg, (WIDTH/2 - 25, HEIGHT/2))
-            # 살짝 sleep 주기
-
-
+            self.gameDisplay.blit(ITEM, (40, 50))
+            self.gameDisplay.blit(ITEM, (140, 50))
+            self.gameDisplay.blit(ITEM, (240, 50))
+            self.gameDisplay.blit(ITEM, (340, 50))
+            self.gameDisplay.blit(ITEM, (440, 50))
+            self.gameDisplay.blit(ITEM, (540, 50))
+            self.gameDisplay.blit(self.player.playerImg, (WIDTH/2 - 25, HEIGHT * 2/3))
 
     def movePlayer(self, posX, posY):
         nxtPosX = self.player.posX + posX
@@ -119,8 +129,11 @@ class Game:
             self.map[nxtPosY][nxtPosX] = '0'
         if self.map[nxtPosY][nxtPosX] == 'E' and self.allCollected:
             self.completed = True
+            global gameFlag
+            gameFlag = 2
             END_SOUND.play()
-
+            END_SOUND.set_volume(0.5)
+            END_SOUND.fadeout(23500) # 23.5초
 
 def parseMap(map):
     for i in range(len(map)):
@@ -141,8 +154,32 @@ def getMap(file):
     map.close()
     return ret
 
+def getClickedItem():
+    mouse_pos = pygame.mouse.get_pos()
+    print(mouse_pos)
+    if mouse_pos[0] >= 50 and mouse_pos[0] <= 80 :
+        if mouse_pos[1] >= 55 and mouse_pos[1] <= 90 :
+            return 1
+    if mouse_pos[0] >= 150 and mouse_pos[0] <= 180 :
+        if mouse_pos[1] >= 55 and mouse_pos[1] <= 90 :
+            return 2
+    if mouse_pos[0] >= 250 and mouse_pos[0] <= 280 :
+        if mouse_pos[1] >= 55 and mouse_pos[1] <= 90 :
+            return 3
+    if mouse_pos[0] >= 350 and mouse_pos[0] <= 380 :
+        if mouse_pos[1] >= 55 and mouse_pos[1] <= 90 :
+            return 4
+    if mouse_pos[0] >= 450 and mouse_pos[0] <= 480 :
+        if mouse_pos[1] >= 55 and mouse_pos[1] <= 90 :
+            return 5
+    if mouse_pos[0] >= 550 and mouse_pos[0] <= 580 :
+        if mouse_pos[1] >= 55 and mouse_pos[1] <= 90 :
+            return 6
+    return 0
+
 if __name__ == "__main__":
     ## 게임 창 설정 ##
+    gameFlag = 1
     map = getMap("easy.map")
     GameDisplay = pygame.display.set_mode((WIDTH,HEIGHT))
     GameDisplay.fill(WHITE)
@@ -166,4 +203,6 @@ if __name__ == "__main__":
                     game.movePlayer(-1, 0)
                 if event.key == pygame.K_DOWN:
                     game.movePlayer(0, -1)
+            if event.type == pygame.MOUSEBUTTONDOWN and gameFlag == 2:
+                print(getClickedItem())
         game.drawMap()
